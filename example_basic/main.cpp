9597bcba10d7a97d11b9b01c344252a299cded63
sky3141 <sky3141@qq.com>
2026-02-08 02:07:27 +0800

    feat: 添加 clang-format 并格式化现有代码

--- example_basic/main.cpp
+++ example_basic/main.cpp
@@ -1,28 +1,28 @@
-#include <iostream>
-#include <iomanip>
 #include "metacar/metacar.hpp"
+#include <iomanip>
+#include <iostream>
 
 int main() {
-    metacar::SceneAPI api;
+  metacar::SceneAPI api;
 
-    std::cout << "Waiting for simulator connection...\n";
-    api.connect();
-    std::cout << "Connected!\n";
+  std::cout << "Waiting for simulator connection...\n";
+  api.connect();
+  std::cout << "Connected!\n";
 
-    while (auto msg = api.step()) {
-        const auto& pose = msg->pose_gnss;
-        double speed = msg->main_vehicle.speed;
+  while (auto msg = api.step()) {
+    const auto &pose = msg->pose_gnss;
+    double speed = msg->main_vehicle.speed;
 
-        std::cout << std::fixed << std::setprecision(2)
-                  << "pos=(" << pose.pos_x << ", " << pose.pos_y << ", " << pose.pos_z << ")  "
-                  << "speed=" << speed << " m/s\n";
+    std::cout << std::fixed << std::setprecision(2) << "pos=(" << pose.pos_x << ", " << pose.pos_y
+              << ", " << pose.pos_z << ")  "
+              << "speed=" << speed << " m/s\n";
 
-        metacar::VehicleControl ctrl;
-        ctrl.throttle = 0.1;
-        ctrl.gear     = metacar::GearMode::DRIVE;
-        api.set_vehicle_control(ctrl);
-    }
+    metacar::VehicleControl ctrl;
+    ctrl.throttle = 0.1;
+    ctrl.gear = metacar::GearMode::DRIVE;
+    api.set_vehicle_control(ctrl);
+  }
 
-    std::cout << "Simulation ended.\n";
-    return 0;
+  std::cout << "Simulation ended.\n";
+  return 0;
 }
